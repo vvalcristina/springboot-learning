65b9e23d75ae31188fdca4075bd4189e3bfacc3d
valeria <silvvavaleria@gmail.com>
2020-05-03 12:47:36 -0300

    Variável de ambiente

--- src/main/java/com/digitalinnovationone/springboot/controller/AppController.java
+++ src/main/java/com/digitalinnovationone/springboot/controller/AppController.java
@@ -10,10 +10,19 @@ public class AppController {
     @Value("${app.message}")
     private String appMessage;
 
+    //Variável de ambiente
+    @Value("${ENV_DB_URL:NENHUMA}")
+    private String dbEnvironmentVariable;
+
     //Toda vez que subirmos a aplicação na porta 8080
     //Ele exibirá no browser a menssagem
     @GetMapping("/")
     public String getAppMessage(){
         return appMessage;
+    }
+
+    @GetMapping("/envVariable")
+    public String getDbEnvironmentVariable(){
+        return "A seguinte variável de ambiente foi passada: " + dbEnvironmentVariable;
     }
 }
